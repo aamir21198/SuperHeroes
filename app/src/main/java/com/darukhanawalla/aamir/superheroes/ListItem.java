ebf44740ff21b079e63f1168ffce2392970048a8
aamir21198 <aamir@darukhanawalla.com>
2017-11-01 21:56:40 +0530

    Initial commit

---
+++ app/src/main/java/com/darukhanawalla/aamir/superheroes/ListItem.java
@@ -0,0 +1,29 @@
+package com.darukhanawalla.aamir.superheroes;
+
+/**
+ * Created by aamir on 1/11/17.
+ */
+
+public class ListItem {
+    private String head;
+    private String desc;
+    private String imageUrl;
+
+    public ListItem(String head, String desc, String imageUrl) {
+        this.head = head;
+        this.desc = desc;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
---
+++ app/src/main/java/com/darukhanawalla/aamir/superheroes/MyAdapter.java
@@ -0,0 +1,73 @@
+package com.darukhanawalla.aamir.superheroes;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by aamir on 1/11/17.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+    private List<ListItem> listItems;
+    private Context context;
+
+    public MyAdapter(List<ListItem> listItems, Context context) {
+        this.listItems = listItems;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final ListItem listItem = listItems.get(position);
+        holder.textViewHead.setText(listItem.getHead());
+        holder.textViewDesc.setText(listItem.getDesc());
+        Picasso.with(context).load(listItem.getImageUrl()).into(holder.imageView);
+
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, listItem.getHead().toUpperCase(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewHead;
+        TextView textViewDesc;
+        ImageView imageView;
+        LinearLayout linearLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewHead=(TextView) itemView.findViewById(R.id.textViewHead);
+            textViewDesc=(TextView) itemView.findViewById(R.id.textViewDesc);
+            imageView=(ImageView) itemView.findViewById(R.id.imageView);
+            linearLayout=(LinearLayout) itemView.findViewById(R.id.linearLayout);
+        }
+    }
+}
